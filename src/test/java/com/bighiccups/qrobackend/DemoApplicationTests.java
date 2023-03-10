352290ea5b9b370ebdcd612f9b01dc17fa9735cf
Bruno C. Manso <mrbrunomanso@gmail.com>
2023-03-10 12:34:11 -0300

    Spring boot init

---
+++ src/test/java/com/bighiccups/qrobackend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bighiccups.qrobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
